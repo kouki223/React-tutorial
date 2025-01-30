2261a67c4937bc37eaf9831f3884d0e46eeea552
kouki223 <kouki.1818194@gmail.com>
2025-01-31 00:04:51 +0900

    docs: 実際にコードかいてます。

--- react-turorial-youtube/src/App.js
+++ react-turorial-youtube/src/App.js
@@ -2,18 +2,18 @@ import { useState } from "react";
 import TodoList from "./TodoList";
 
 function App() {
-    const [todos,setTodos] = useState(["Todo1","Todo2"]);
-//setTodos
-//useState
+    const [todos, setTodos] = useState([
+      { id: 1, name: "Todo1", completed: false},
+    ]);
 
   return (
-   <div>
+   <>
     <TodoList todos={todos} />
     <input type="text" />
     <button>タスクの追加</button>
     <button>完了したタスクの削除</button>
     <div>残りのタスク:0</div>
-  </div>
+  </>
   );
 }
 
--- react-turorial-youtube/src/Todo.js
+++ react-turorial-youtube/src/Todo.js
@@ -1,7 +1,13 @@
 import React from "react"
 
 const Todo = ({ todo }) => {
-    return <div>{todo}</div>;
+    return 
+    <div>
+        <label>
+            <input type="checkbox" checked={todo.completed}/>
+        </label>
+        {todo.name}
+    </div>;
 };
 
 export default Todo;
